cfc39bf5287fb58281e1312b8277d8ded9e9635c
Danger <51889207+DangerDan88@users.noreply.github.com>
2021-02-08 16:51:54 -0700

    installed correct router package and got css variables to work globally

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import GlobalStyles from "../src/GlobalStyles";
 import Container from "../src/components/Container";
 import Header from "../src/components/Header";
+import Game from "../src/components/Game";
+import GameOver from "../src/components/GameOver";
+import HighScores from "../src/components/HighScores";
+import Home from "../src/components/Home";
 
 function App() {
   return (
